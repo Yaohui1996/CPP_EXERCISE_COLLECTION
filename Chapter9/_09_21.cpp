ae325efcad256fd71e7e0e040ae3d710b2f39156
Yaohui1996 <zggdscutlyh@163.com>
2021-02-26 16:35:00 +0800

    2021/02/26 update

--- Chapter9/_09_21.cpp
+++ Chapter9/_09_21.cpp
@@ -2,4 +2,26 @@
 // Created by Yaohui Li on 2021/2/22.
 //
 
-int main() { return 0; }
+#include <vector>
+#include <iostream>
+#include <list>
+#include <string>
+
+using namespace std;
+
+int main() {
+    vector<string> vec;
+    auto iter0 = vec.begin();
+    string word0;
+    while (cin >> word0) {
+        iter0 = vec.insert(iter0, word0);
+    }
+
+    list<string> lst;
+    auto iter = lst.begin();
+    string word;
+    while (cin >> word) {
+        iter = lst.insert(iter, word);
+    }
+    return 0;
+}
