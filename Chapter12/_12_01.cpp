875d27e824e563cddcd9736b927422ca2d245898
Yaohui1996 <zggdscutlyh@163.com>
2021-07-07 17:07:28 +0800

    20210707update

--- Chapter12/_12_01.cpp
+++ Chapter12/_12_01.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <memory>
+#include <string>
 
 using std::cout;
 using std::endl;
 using std::shared_ptr;
+using std::string;
 using std::unique_ptr;
 using std::weak_ptr;
+using std::make_shared;
 
 int main()
 {
@@ -16,6 +19,19 @@ int main()
     cout << *p << endl;
 
     delete p;
+
+    shared_ptr<string> p2 = make_shared<string>("chushihua string");
+
+    cout << *p2 << endl;
+
+    if (p2 && p2->empty())
+        *p2 = "hi";
+    
+    cout << *p2 << endl;
+
+    *p2 = "hhhhh";
+    cout << *p2 << endl;
+
 
     return 0;
 }
