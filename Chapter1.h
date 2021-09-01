73ca6bcb9de3283f7210a7908d562b5f8373265a
Yaohui1996 <zggdscutlyh@163.com>
2021-09-01 22:24:57 +0800

    20210901update

---
+++ Chapter1.h
@@ -0,0 +1,44 @@
+#ifndef YH_CXX_PRIMER_CHAPTER1_H
+#define YH_CXX_PRIMER_CHAPTER1_H
+
+class Chapter1 {
+public:
+    Chapter1();
+    ~Chapter1();
+
+    bool run_all_exercise();
+
+private:
+    int exercise_01();
+    int exercise_02();
+    int exercise_03();
+    int exercise_04();
+    int exercise_05();
+    int exercise_06();
+    int exercise_07();
+    int exercise_08();
+    int exercise_09();
+    int exercise_10();
+    int exercise_11();
+    int exercise_12();
+    int exercise_13();
+    int exercise_14();
+    int exercise_15();
+    int exercise_16();
+    int exercise_17();
+    int exercise_18();
+    int exercise_19();
+    int exercise_20();
+    int exercise_21();
+    int exercise_22();
+    int exercise_23();
+    int exercise_24();
+    int exercise_25();
+    int exercise_26();
+    int exercise_27();
+    int exercise_28();
+    int exercise_29();
+    int exercise_30();
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "Chapter1.h"
+
+int main() {
+    Chapter1 chap1;
+    if (!chap1.run_all_exercise()) {
+        return -1;
+    }
+    return 0;
+}
