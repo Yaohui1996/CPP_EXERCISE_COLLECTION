ae85134bbd3572e3e2f2b4c42bd75aa8872ed15d
Yaohui1996 <zggdscutlyh@163.com>
2022-05-02 10:17:40 +0800

    10-18 to 10-19

---
+++ Chapter-10/10-18/10-18.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Yaohui Li on 2022/5/2.
+//
+
+// 练习10.18：重写biggies，用partition代替find_if。
+// 我们在10.3.1节练习10.13（第345页）中介绍了partition算法。
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void biggies(vector<string>& v, string::size_type sz) {
+    auto f = [sz](const string& s) -> bool { return s.size() >= sz; };
+    auto p = partition(v.begin(), v.end(), f);
+    std::for_each(v.begin(), p,
+                  [](const string& s) -> void { cout << s << " "; });
+}
+
+int main() {
+    vector<string> v = {"the",  "quick", "red",  "fox", "jumps",
+                        "over", "the",   "slow", "red", "turtle"};
+    biggies(v, 5);
+    return 0;
+}
---
+++ Chapter-10/10-19/10-19.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Yaohui Li on 2022/5/2.
+//
+
+// 练习10.19：用stable_partition重写前一题的程序，与stable_sort类似，在划分后的序列中维持原有元素的顺序。
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void biggies(vector<string>& v, string::size_type sz) {
+    auto f = [sz](const string& s) -> bool { return s.size() >= sz; };
+    auto p = stable_partition(v.begin(), v.end(), f);
+    std::for_each(v.begin(), p,
+                  [](const string& s) -> void { cout << s << " "; });
+}
+
+int main() {
+    vector<string> v = {"the",  "quick", "red",  "fox", "jumps",
+                        "over", "the",   "slow", "red", "turtle"};
+    biggies(v, 5);
+    return 0;
+}
