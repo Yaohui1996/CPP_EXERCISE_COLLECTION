db486a7849e9f34db0a80ff75fdffc539cd3d41f
Yaohui1996 <zggdscutlyh@163.com>
2023-06-04 14:41:51 +0800

    feat: add fig06_10.cpp

---
+++ CPP_HOW_TO_PROGRAM_NINTH/exe/fig06_10.cpp
@@ -0,0 +1,21 @@
+#include <cstdint>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main() {
+  uint32_t seed = 0;
+  cout << "请输入随机数种子: ";
+  cin >> seed;
+  srand(seed);
+  // srand(static_cast<uint32_t>(time(0)));
+  for (uint32_t counter = 1; counter <= 10; ++counter) {
+    cout << setw(10) << (1 + rand() % 6);
+    if (counter % 5 == 0) {
+      cout << endl;
+    } // end if
+  }   // end for
+
+  return 0;
+}
