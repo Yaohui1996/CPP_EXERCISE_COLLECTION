99253bf5fff0eb3d5e5ead427dc61ca4dd00ae9f
Yaohui1996 <zggdscutlyh@163.com>
2023-06-05 20:01:25 +0800

    feat: add fig06_23.cpp

---
+++ CPP_HOW_TO_PROGRAM_NINTH/exe/fig06_23.cpp
@@ -0,0 +1,20 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int square(const int x) {
+  cout << "square of integer " << x << " is " << endl;
+  return x * x;
+}
+
+double square(const double x) {
+  cout << "square of double " << x << " is " << endl;
+  return x * x;
+}
+
+int main() {
+  cout << square(7) << endl;
+  cout << square(7.5) << endl;
+  return 0;
+}
