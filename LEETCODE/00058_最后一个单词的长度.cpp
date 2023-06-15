14788234877a00eeab78ca74917a8ca7c57ceab6
Yaohui1996 <zggdscutlyh@163.com>
2023-06-15 18:37:22 +0800

    feat: add LC problems

---
+++ LEETCODE/00058_最后一个单词的长度.cpp
@@ -0,0 +1,29 @@
+//* 58.最后一个单词的长度.cpp
+//* https://leetcode.cn/problems/length-of-last-word/
+
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        //* 跳过末尾的空格
+        int i = s.size() - 1;
+        for(; i >= 0; --i){
+            const auto ch = s[i];
+            if(ch == ' '){
+                continue;
+            }
+            break;
+        }
+        //* 开始计数
+        int ret = 0;
+        for(; i >= 0; --i){
+            const auto ch = s[i];
+            if(ch != ' '){
+                ++ret;
+                continue;
+            }
+            return ret;
+        }
+        return ret;
+    }
+};
