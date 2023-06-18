473788b7b4df245ac84349a591cd19790ab1c446
Yaohui1996 <zggdscutlyh@163.com>
2023-06-18 14:37:39 +0800

    feat: add lc 171

---
+++ LEETCODE/00171_Excel表序号.cpp
@@ -0,0 +1,14 @@
+//* 171.Excel表序号.cpp
+//* https://leetcode.cn/problems/excel-sheet-column-number/
+
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int ret = 0;
+        for(int i = columnTitle.size() - 1; i >= 0; --i){
+            ret += ((int)columnTitle[i] - 64) * pow(26, columnTitle.size() - 1 - i);
+        }
+        return ret;
+    }
+};
