0f45338de5e0df6af831e589c3652c0b95823c6a
Yaohui1996 <zggdscutlyh@163.com>
2023-07-02 11:30:28 +0800

    feat: 新增257和290

---
+++ LEETCODE/00257_二叉树的所有路径.cpp
@@ -0,0 +1,47 @@
+//* 257.二叉树的所有路径.cpp
+//* https://leetcode.cn/problems/binary-tree-paths/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    std::string convert_path_to_str(const std::vector<int>& path)const{
+        std::stringstream ss;
+        for(size_t i = 0; i != path.size(); ++i){
+            ss << std::to_string(path[i]);
+            if(i != path.size() - 1){
+                ss << "->";
+            }
+        }
+        return ss.str();
+    }
+    void DFS(const TreeNode* const node, std::vector<int> path, std::vector<std::string>& result)const{
+        path.push_back(node->val);
+        if(!node->left && !node->right){
+            result.push_back(convert_path_to_str(path));
+        }
+        if(node->left){
+            DFS(node->left,path,result);
+        }
+        if(node->right){
+            DFS(node->right,path,result);
+        }
+        
+    }
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        std::vector<std::string> result;
+        DFS(root,{},result);
+        return result;
+    }
+};
