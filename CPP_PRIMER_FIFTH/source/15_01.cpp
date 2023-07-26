3d53defef11189328eabfc9b35ae09d9ec6db895
Yaohui1996 <zggdscutlyh@163.com>
2023-07-26 11:54:04 +0800

    feat: update 15_01.cpp

--- CPP_PRIMER_FIFTH/source/15_01.cpp
+++ CPP_PRIMER_FIFTH/source/15_01.cpp
@@ -3,33 +3,54 @@
 #include <string>
 
 class Quote {
- public:
-  std::string isbn() const;
-  virtual double net_price(std::size_t n) const;
+public:
+    Quote() = default;
+    Quote(const std::string& book, const double sales_price) : bookNo(book), price(sales_price) {}
+    virtual ~Quote() = default;
+
+public:
+    std::string isbn() const;
+    virtual double net_price(std::size_t n) const;
+
+private:
+    std::string bookNo;
+
+protected:
+    double price = 0.0;
 };
 
 class Bulk_quote : public Quote {
- public:
-  double net_price(std::size_t n) const override;
+public:
+    Bulk_quote() = default;
+    Bulk_quote(const std::string&, const double, const std::size_t, const double);
+
+public:
+    double net_price(std::size_t n) const override;
+
+private:
+    std::size_t min_qty = 0;
+    double discount = 0.0;
 };
 
 double print_total(std::ostream& os, const Quote& item, size_t n) {
-  double ret = item.net_price(n);
-  os << "ISBN:" << item.isbn() << "#sold: " << n << "total due: " << ret
-     << std::endl;
-  return ret;
+    double ret = item.net_price(n);
+    os << "ISBN:" << item.isbn() << "#sold: " << n << "total due: " << ret << std::endl;
+    return ret;
 }
 
 int main() {
-  Quote quote;
-  Bulk_quote bulk_quote;
-  print_total(std::cout, quote, 1);
-  print_total(std::cout, bulk_quote, 5);
-  return 0;
+    Quote quote;
+    Bulk_quote bulk_quote;
+    print_total(std::cout, quote, 1);
+    print_total(std::cout, bulk_quote, 5);
+    return 0;
 }
 
-std::string Quote::isbn() const { return "example isbn"; }
+std::string Quote::isbn() const { return bookNo; }
 
-double Quote::net_price(std::size_t n) const { return 1.0 * n; }
+double Quote::net_price(std::size_t n) const { return price * n; }
 
 double Bulk_quote::net_price(std::size_t n) const { return 5.0 * n; }
+
+Bulk_quote::Bulk_quote(const std::string& book, const double p, const std::size_t qty, const double disc) : Quote(book, p), min_qty(qty), discount(disc) {}
+
