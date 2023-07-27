40bdf3f5429163d1108df9f63895f156b1df6c75
Yaohui1996 <zggdscutlyh@163.com>
2023-07-28 07:54:22 +0800

    feat: 指定编译器用clang

--- release.sh
+++ release.sh
@@ -7,5 +7,5 @@ fi
 echo "create build..."
 mkdir build
 echo "create build finished!"
-cmake -S . -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
 make -C build -j10
