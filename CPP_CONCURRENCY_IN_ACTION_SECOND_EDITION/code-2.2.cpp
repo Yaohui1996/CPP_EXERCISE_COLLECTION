425ae1fad6e0b70ef7a53f8161d7e26765c0920e
Yaohui1996 <zggdscutlyh@163.com>
2023-08-01 14:55:33 +0800

    feat: add code 2.2 and 2.3

---
+++ CPP_CONCURRENCY_IN_ACTION_SECOND_EDITION/code-2.2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <thread>
+
+struct func {
+  int &var_;
+  func(int &v) : var_(v) {}
+  void operator()() {
+    for (std::size_t i = 0; i != 10000; ++i) {
+      std::cout << var_ << std::endl;
+    }
+  }
+};
+
+void f() {
+  int some_local_state = 0;
+  func my_func(some_local_state);
+  std::thread t(my_func);
+  try {
+    std::cout << "do something in current thread..." << std::endl;
+  } catch (...) {
+    t.join();
+    throw;
+  }
+  t.join();
+}
+
+int main() {
+  f();
+  return 0;
+}
