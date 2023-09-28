6b17ec3f078b6d06cc88f517580592ba04aed887
Yaohui1996 <zggdscutlyh@163.com>
2023-09-28 10:37:21 +0800

    feat: update 1.0

--- ACCELERATED_CPP/exercise_1_0.cpp
+++ ACCELERATED_CPP/exercise_1_0.cpp
@@ -4,14 +4,18 @@
 
 int main()
 {
-    const uint32_t width = 20;
+    uint32_t width = 20;
     std::cout << "Please enter your first name: ";
     std::string name;
     std::cin >> name;
+    if (name.size() + 12 > width)
+    {
+        width = name.size() + 12;
+    }
     std::cout << std::string(width, '*') << std::endl;
     std::cout << "*" << std::string(width - 2, ' ') << "*" << std::endl;
-    std::cout << "* Hello, " << name << "!" << std::string(width - 2 - 8 - name.size() - 2, ' ') << " *" << std::endl;
+    std::cout << "* Hello, " << name << "!" << std::string(width - 12 - name.size(), ' ') << " *" << std::endl;
     std::cout << "*" << std::string(width - 2, ' ') << "*" << std::endl;
-    std::cout << std::string(20, '*') << std::endl;
+    std::cout << std::string(width, '*') << std::endl;
     return 0;
 }
