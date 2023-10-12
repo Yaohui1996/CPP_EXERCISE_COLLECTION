bc6a74dc1817e523e74067957ff8b288e5d226ee
Yaohui1996 <zggdscutlyh@163.com>
2023-10-12 11:24:33 +0800

    feat: update exe 2.2

--- ACCELERATED_CPP/exercise_2_2.cpp
+++ ACCELERATED_CPP/exercise_2_2.cpp
@@ -10,18 +10,21 @@ int main()
     const std::string greeting = "Hello, " + name + "!";
     // std::cout << "Hello, " << name << "!" << std::endl;
 
-    const int32_t pad = 0;
-    const int32_t rows = pad * 2 + 3;
+    const int32_t left = 2;
+    const int32_t right = 3;
+    const int32_t top = 1;
+    const int32_t bottom = 0;
+    const int32_t rows = top + bottom + 3;
 
     std::cout << std::endl;
 
     for (int32_t r = 0; r != rows; ++r)
     {
-        const std::string::size_type cols = greeting.size() + pad * 2 + 2;
+        const std::string::size_type cols = greeting.size() + left + right + 2;
         std::string::size_type c = 0;
         while (c != cols)
         {
-            if (r == pad + 1 && c == pad + 1)
+            if (r == top + 1 && c == left + 1)
             {
                 std::cout << greeting;
                 c += greeting.size();
