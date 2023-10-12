6a819d68ec939cd857405c7a27cac2dd2c0c7194
Yaohui1996 <zggdscutlyh@163.com>
2023-10-12 11:14:33 +0800

    feat: update exe 2.0

--- ACCELERATED_CPP/exercise_2_0.cpp
+++ ACCELERATED_CPP/exercise_2_0.cpp
@@ -4,18 +4,38 @@
 
 int main()
 {
-    uint32_t width = 20;
     std::cout << "Please enter your first name: ";
     std::string name;
     std::cin >> name;
-    if (name.size() + 12 > width)
+    const std::string greeting = "Hello, " + name + "!";
+    // std::cout << "Hello, " << name << "!" << std::endl;
+
+    const int32_t pad = 1;
+    const int32_t rows = pad * 2 + 3;
+
+    std::cout << std::endl;
+
+    for (int32_t r = 0; r != rows; ++r)
     {
-        width = name.size() + 12;
+        const std::string::size_type cols = greeting.size() + pad * 2 + 2;
+        std::string::size_type c = 0;
+        while (c != cols)
+        {
+            if (r == pad + 1 && c == pad + 1)
+            {
+                std::cout << greeting;
+                c += greeting.size();
+            }
+            else
+            {
+                if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1)
+                    std::cout << "*";
+                else
+                    std::cout << " ";
+                ++c;
+            }
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::string(width, '*') << std::endl;
-    std::cout << "*" << std::string(width - 2, ' ') << "*" << std::endl;
-    std::cout << "* Hello, " << name << "!" << std::string(width - 12 - name.size(), ' ') << " *" << std::endl;
-    std::cout << "*" << std::string(width - 2, ' ') << "*" << std::endl;
-    std::cout << std::string(width, '*') << std::endl;
     return 0;
 }
