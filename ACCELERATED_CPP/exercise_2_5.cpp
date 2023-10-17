8c53abbc91ebc7a4613b7418259e389d9616fce6
Yaohui1996 <zggdscutlyh@163.com>
2023-10-17 19:42:18 +0800

    feat: add square

--- ACCELERATED_CPP/exercise_2_5.cpp
+++ ACCELERATED_CPP/exercise_2_5.cpp
@@ -3,5 +3,27 @@
 
 int main()
 {
+    // 正方形
+    int d1 = 0;
+    std::cout << "请输入正方形的边长: ";
+    std::cin >> d1;
+    for (int r = 0; r != d1; ++r)
+    {
+        if (r == 0 || r == d1 - 1)
+        {
+            std::cout << std::string(d1, '*');
+        }
+        else
+        {
+            std::cout << "*";
+            std::cout << std::string(d1 - 2, ' ');
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+
+    // 长方形
+
+    // 三角形
     return 0;
 }
