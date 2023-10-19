6bc9041c8f6281dd0dabc3e1acdcb4725d49b802
Yaohui1996 <zggdscutlyh@163.com>
2023-10-19 11:21:58 +0800

    feat: add exe 2.9

--- ACCELERATED_CPP/exercise_2_9.cpp
+++ ACCELERATED_CPP/exercise_2_9.cpp
@@ -4,5 +4,23 @@
 int main()
 {
     // 返回较大的数
+    double a = 0;
+    double b = 0;
+    std::cout << "请输入数值a: ";
+    std::cin >> a;
+    std::cout << "请输入数值b: ";
+    std::cin >> b;
+    if (a > b)
+    {
+        std::cout << "较大的数值是: " << a << std::endl;
+    }
+    else if (a < b)
+    {
+        std::cout << "较大的数值是: " << b << std::endl;
+    }
+    else
+    {
+        std::cout << "两个数一样大!" << b << std::endl;
+    }
     return 0;
 }
