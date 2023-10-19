12bdf32548585978d36967bae7079a486dab8629
Yaohui1996 <zggdscutlyh@163.com>
2023-10-19 11:25:18 +0800

    feat: add exe 2.10

---
+++ ACCELERATED_CPP/exercise_2_10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    // 对std::的每一次使用进行解释
+    int k = 0;
+    while (k != 10)
+    {
+        using std::cout;
+        cout << "*";
+    }
+    std::cout << std::endl;
+    return 0;
+}
