73b4c1a9efd8dca24ec4db379a460388f4af0869
Yaohui1996 <zggdscutlyh@163.com>
2023-12-01 19:49:43 +0800

    bugfix: 修复类型名错误

--- CPP_TEMPLATES/basics/printcoll.hpp
+++ CPP_TEMPLATES/basics/printcoll.hpp
@@ -3,7 +3,7 @@
 template <typename T> void printcoll(T const &coll)
 {
     typename T::const_iterator pos;
-    typename T::const_iteratop end(coll.end());
+    typename T::const_iterator end(coll.end());
     for (pos = coll.begin(); pos != end; ++pos)
     {
         std::cout << *pos << " ";
