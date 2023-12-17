5d49e234394d826908edfac0fb637456c3deb89e
Yaohui1996 <zggdscutlyh@163.com>
2023-12-17 12:10:04 +0800

    feat: add test omp

---
+++ OTHERS/test_openmp/test_omp.cpp
@@ -0,0 +1,32 @@
+#include <cstdint>
+#include <iostream>
+#include <omp.h>
+#include <string>
+
+bool make_thread(int n)
+{
+    std::cout << "thread: " << n << " is running..." << std::endl;
+    int a = 0;
+    while (true)
+    {
+
+        if (a == 100)
+        {
+            a = 0;
+        }
+        ++a;
+    }
+    return true;
+}
+
+int main(int argc, const char *argv[])
+{
+    int32_t thread_cnt = std::stoi(argv[1]);
+    std::cout << "thread_cnt: " << thread_cnt << std::endl;
+    #pragma omp parallel for schedule(dynamic) num_threads(thread_cnt)
+    for (int32_t i = 0; i < thread_cnt; ++i)
+    {
+        make_thread(i);
+    }
+    return 0;
+}
